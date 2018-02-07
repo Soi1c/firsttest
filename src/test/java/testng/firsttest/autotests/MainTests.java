659cf6e25caae8031ae7a8cbadca709b2b55102d
Pestov Sergey <sergey.pestov@corp.codeinside.ru>
2018-02-07 14:47:51 +0300

    Initial commit

---
+++ src/test/java/testng/firsttest/autotests/MainTests.java
@@ -0,0 +1,24 @@
+package testng.firsttest.autotests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import java.util.concurrent.TimeUnit;
+
+public class MainTests {
+
+    protected static WebDriver driver;
+
+    @BeforeTest
+    public void beforeEachTest() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterTest
+    public void afterEachTest() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/testng/firsttest/autotests/yaRu/WeatherInPenza.java
@@ -0,0 +1,19 @@
+package testng.firsttest.autotests.yaRu;
+
+import testng.firsttest.autotests.MainTests;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class WeatherInPenza extends MainTests {
+
+    @Test
+    public void weatherInPenzaFirstLink() {
+        driver.get("https://ya.ru/");
+        driver.findElement(By.id("text")).sendKeys("погода пенза", Keys.ENTER);
+        WebElement firstLink =  driver.findElements(By.className("serp-item")).get(0);
+        Assert.assertEquals(firstLink.findElement(By.cssSelector(".link")).getText().contains("Погода в Пензе"), true);
+    }
+}
