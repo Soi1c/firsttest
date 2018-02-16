8d9c60d7c2d7a65110d765b2237f5c7f62591b93
Sergey Pestov <sergey.pestov@corp.codeinside.ru>
2018-02-16 09:48:39 +0300

    re #master Выбор английского языка из списка селектором, убраны комментарии, исправил имя переменной

--- src/homework/h1/autotests/yaRu/ChangeLanguage.java
+++ src/homework/h1/autotests/yaRu/ChangeLanguage.java
@@ -11,21 +11,17 @@ public class ChangeLanguage extends MainTests {
     @Test
     public void changeLanguageOnYaRu() {
         driver.get("https://yandex.ru/");
-        //Настройка языка
         driver.findElement(By.cssSelector("a[data-statlog=\"head.settings\"]")).click();
         driver.findElement(By.cssSelector("a[data-statlog=\"head.settings.other\"]")).click();
         driver.findElement(By.cssSelector("a[data-statlog=\"tabs.lang\"]")).click();
-        if (driver instanceof JavascriptExecutor) {
-            ((JavascriptExecutor) driver)
-                    .executeScript("$('select[name=intl]').val('en').closest('form').submit()");
-        }
-
-        //Проверка языка
+        driver.findElement(By.cssSelector("div[class=\"option__content\"] > div > button ")).click();
+        driver.findElement(By.xpath("//span[@class=\"select__text\" and text() = \"English\"]")).click();
+        driver.findElement(By.cssSelector("button[type = \"submit\"]")).click();
         driver.findElement(By.cssSelector("a[data-statlog=\"head.settings\"]")).click();
         driver.findElement(By.cssSelector("a[data-statlog=\"head.settings.other\"]")).click();
         driver.findElement(By.cssSelector("a[data-statlog=\"tabs.lang\"]")).click();
-        String s = driver.findElement(By.cssSelector("div[class=\"option__content\"] > div > button > span[class=\"button__text\"]")).getText();
+        String localesSelectButtonText = driver.findElement(By.cssSelector("div[class=\"option__content\"] > div > button > span[class=\"button__text\"]")).getText();
 
-        Assert.assertEquals(s, "English");
+        Assert.assertEquals(localesSelectButtonText, "English");
     }
 }
